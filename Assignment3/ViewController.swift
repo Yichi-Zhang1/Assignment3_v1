1b6ba975931e432cdcb3d3af654230d5c6d8c77d
yichi <yzha0410@student.monash.edu>
2020-10-31 14:13:44 +0800

    Initial Commit

---
+++ Assignment3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment3
+//
+//  Created by admin on 2020/10/31.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
